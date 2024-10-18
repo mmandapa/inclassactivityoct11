f292e80569be1ddd05d5547b156dc19d2cc2d127
maharshimandapati <70028416+maharshimandapati@users.noreply.github.com>
2024-10-17 19:47:11 -0700

    commit

---
+++ personal-website.js
@@ -0,0 +1,15 @@
+document.getElementById('about-btn').onclick = function () {
+    this.style.backgroundColor = '#ff7043';
+};
+
+document.getElementById('projects-btn').onclick = function () {
+    alert('projects coming soon');
+};
+
+document.getElementById('resume-btn').onclick = function () {
+    alert("resume not uploaded yet")
+};
+
+document.getElementById('message-btn').onclick = function () {
+    alert('Thank you for messaging me!');
+};
